6e2e2e43ccb5d868942ad107a855a9bc173af1e4
Qiusheng Wu <giswqs@gmail.com>
2021-12-11 23:09:10 -0500

    Updated homepage

--- apps/home.py
+++ apps/home.py
@@ -3,4 +3,5 @@ import streamlit as st
 
 def app():
     st.title("Home")
-    st.text("Welcome to Scholar Web App")
+    st.text("Welcome to Scholar Web App. Click on the left sidebar menu to explore.")
+    st.image("https://i.imgur.com/ZNUJ9fF.gif")

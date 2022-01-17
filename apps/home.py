e39773cffe2083cc4ab6a630f4fef13150b44ac5
Qiusheng Wu <giswqs@gmail.com>
2022-01-17 11:36:23 -0500

    Added disclaimer

--- apps/home.py
+++ apps/home.py
@@ -6,4 +6,11 @@ def app():
     st.text(
         "Welcome to the Scholar Web App. Click on the left sidebar menu to explore."
     )
+    markdown = """
+    Disclaimer: The data records are pulled from the [Dimensions.ai database](https://app.dimensions.ai), which might be be the most complete bibliometric database.
+    We plan to incorporate [Scopus](https://www.scopus.com) and [Google Scholar](https://scholar.google.com) in the near future. Don't be surprised if you see that 
+    your publication records are not the same as your Google Scholar profile. This is a very preliminary version. A lot more features will be added in the future.
+    We would welcome any feedback. Please send feedback to Qiusheng Wu (qwu18@utk.edu).
+    """
+    st.info(markdown)
     st.image("https://i.imgur.com/ZNUJ9fF.gif")
